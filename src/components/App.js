39c491816927c5a1cb08b3d495b37e584fc9e65e
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-12 16:38:15 -0600

    add Home, fix urls

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,18 @@ import {Main} from './Main'
 import '../styles/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <Main/>
-      </div>
-    );
-  }
+    state = {
+        isLoggedIn: false
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header isLoggedIn={this.state.isLoggedIn}/>
+                <Main isLoggedIn={this.state.isLoggedIn}/>
+            </div>
+        );
+    }
 }
 
 export default App;

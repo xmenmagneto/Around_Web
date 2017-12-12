52ff67b6f20baadf72548056a3f0569138bc74c3
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-12 11:12:41 -0600

    change Login form style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {Register} from "./Register";
+import { Login } from "./Login";
 
 export class Main extends Component {
     render() {
         return (
             <div className="main">
-                <Register/>
+                <Login/>
             </div>
         );
     }

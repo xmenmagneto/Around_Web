1e93266f2a3d5ce166c306c0a94de22f7fc8d86f
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-14 16:00:53 -0600

    loadNearybyPosts after each drag

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -4,10 +4,23 @@ import {POS_KEY} from "../constants"
 import { AroundMarker } from './AroundMarker'
 
 class AroundMap extends React.Component {
+    onDragEnd = () => {
+        const center = this.map.getCenter();
+        const position = { lat: center.lat(), lon: center.lng() };
+        localStorage.setItem(POS_KEY, JSON.stringify(position)); //替换成当前图中央
+        this.props.loadNearbyPosts();
+    }
+
+    getMapRef = (map) => {
+        this.map = map;
+    }
+
     render() {
         const pos = JSON.parse(localStorage.getItem(POS_KEY)); //JSON.parse -> 转换成object
         return (
             <GoogleMap
+                onDragEnd={this.onDragEnd}
+                ref={this.getMapRef}
                 defaultZoom={11}
                 defaultCenter={{ lat: pos.lat, lng: pos.lon }}
             >
